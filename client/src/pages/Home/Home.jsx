486fe619d188732ee86c770750eb6c41f7a5495f
kavyansh18 <kavyanshkumar18@gmail.com>
2024-06-03 15:51:26 +0530

    auth

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../Components/RightSidebar/RightSidebar'
+import HomeMainbar from '../../Components/HomeMainbar/HomeMainbar'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-container'>
+      <LeftSidebar />
+      <div className="home-conatiner-2">
+        <HomeMainbar />
+        <RightSidebar />
+      </div>
       
     </div>
   )
