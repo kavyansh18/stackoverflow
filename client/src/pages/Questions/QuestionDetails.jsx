d000d8f70e01fc23de7a66507a1e9301e1e5f276
kavyansh18 <kavyanshkumar18@gmail.com>
2024-06-24 21:20:32 +0530

    completed

--- client/src/pages/Questions/QuestionDetails.jsx
+++ client/src/pages/Questions/QuestionDetails.jsx
@@ -77,7 +77,7 @@ const QuestionDetails = () => {
   const Navigate = useNavigate();
   const dispatch = useDispatch();
   const location = useLocation();
-  const url = "http://localhost:5500";
+  const url = "https://stackoverflow-kk.onrender.com";
 
   const handlePostAns = (e, answerLength) => {
     e.preventDefault();
